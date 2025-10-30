6e51955f937cfa7a2724a74348d930098543bec5
Manik Chauhan <manikchauhan029@gmail.com>
2025-10-30 18:17:53 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import UserDashboard from "./components/UserDashboard";
+import Login from "./Login";
+import Register from "./Register";
+import NotFound from "./components/NotFound";
+import './index.css';
+// import Quiz from "./components/Quiz";
+import ProfilePage from "./components/ProfilePage";
+import Calendar from "./components/Calender";
+import Reports from "./components/Reports";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<UserDashboard />} />
+        {/* <Route path="/quiz" element={<Quiz />} /> */}
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/calender" element={<Calendar />} />
+        <Route path="/reports" element={<Reports />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/Login.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import namaste from "./images/namaste.png";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isCorrect, setIsCorrect] = useState(true);
+  const navigate = useNavigate();
+
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:8000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        const token = data.token;
+
+        localStorage.setItem("token", token);
+        localStorage.setItem("email", email);
+        navigate("/dashboard");
+        console.log("Login successful!");
+      } else {
+        setIsCorrect(false);
+        console.error("Login failed.");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-20 w-auto"
+            src={namaste}
+            alt="Company Logo"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-md font-bold leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  value={email}
+                  onChange={handleEmailChange}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-md font-bold leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  value={password}
+                  onChange={handlePasswordChange}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            {!isCorrect && (
+              <div className="text-md text-red-500">
+                Your credentials are invalid, please try again.
+              </div>
+            )}
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{" "}
+            <Link
+              to="/register"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Register for free!!
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
